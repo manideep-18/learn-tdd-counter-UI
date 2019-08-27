0cf3158b3891ff67cbaad9bfcbf41b2c73dea533
18121997 <r8manideep@gmail.com>
2019-08-27 13:18:47 +0530

    [Green State] incrementCounter Function Implmented

--- src/Store/CounterStore/index.js
+++ src/Store/CounterStore/index.js
@@ -5,6 +5,8 @@ class CounterStore {
   constructor() {
     this.counter = 0;
   }
-  @action incrementCounter = () => {};
+  @action incrementCounter = () => {
+    this.counter = this.counter + 1;
+  };
 }
 export default CounterStore;
--- src/counterStore.test.js
+++ src/counterStore.test.js
@@ -3,6 +3,7 @@ import CounterStore from "./Store/CounterStore";
 describe("CounterStore testing", () => {
   it("should test increment counter with 0", () => {
     const counterStore = new CounterStore();
-    expect(counterStore.incrementCounter()).toBe(1);
+    counterStore.incrementCounter();
+    expect(counterStore.counter).toBe(1);
   });
 });
