5d859276d319f66435a97a8a497ab4c4f0ad463e
18121997 <r8manideep@gmail.com>
2019-08-27 13:33:18 +0530

    [Green State] IncrementButton rendered

--- src/CounterComponent/index.js
+++ src/CounterComponent/index.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import { CounterComponentBg, IncrementButton } from "./styledComponents";
 
 class CounterComponent extends Component {
   render() {
-    return <div></div>;
+    return (
+      <CounterComponentBg>
+        <IncrementButton>Increment</IncrementButton>
+      </CounterComponentBg>
+    );
   }
 }
 export default CounterComponent;
--- src/CounterComponent/styledComponents.js
+++ src/CounterComponent/styledComponents.js
@@ -1,2 +1,3 @@
 import styled from "styled-components";
+export const CounterComponentBg = styled.div``;
 export const IncrementButton = styled.button``;
